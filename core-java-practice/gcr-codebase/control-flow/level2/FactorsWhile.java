0f4b190e6f873ecf564d55081941f34af35c9705
Sandeep Sisodiy <sandeepthakurtaroli@gmail.com>
2026-06-16 10:40:02 +0530

    [Sandeep] Add: <Added control flow>

---
+++ core-java-practice/gcr-codebase/control-flow/level2/FactorsWhile.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class FactorsWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a positive number: ");
+        int number = sc.nextInt();
+
+        int counter = 1;
+
+        if (number > 0) {
+            System.out.println("Factors are:");
+
+            while (counter < number) {
+
+                if (number % counter == 0) {
+                    System.out.println(counter);
+                }
+
+                counter++;
+            }
+        } else {
+            System.out.println("Enter a positive integer.");
+        }
+
+        sc.close();
+    }
+}
