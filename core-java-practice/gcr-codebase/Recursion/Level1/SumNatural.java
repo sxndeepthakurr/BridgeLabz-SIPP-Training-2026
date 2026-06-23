8086e512790a3656484c06659a3078b65c82f19e
Sandeep Sisodiya <168396929+sxndeepthakurr@users.noreply.github.com>
2026-06-23 10:15:25 +0530

    Delete core-java-practice directory

--- core-java-practice/gcr-codebase/Recursion/Level1/SumNatural.java
+++
@@ -1,9 +0,0 @@
-class SumNatural {
-    static int s(int n){
-        if(n==0) return 0;
-        return n+s(n-1);
-    }
-    public static void main(String[] args){
-        System.out.println(s(5));
-    }
-}
