34b9e8c66599f8ae76871aacfb119388f373d40f
Sandeep Sisodiy <sandeepthakurtaroli@gmail.com>
2026-07-21 09:14:43 +0530

    [Sandeep] Add:Hashing added

--- Collections and DSA/SortingAlgorithms/MergeSort/CustomerOrderValues.java
+++ Collections and DSA/SortingAlgorithms/MergeSort/CustomerOrderValues.java
@@ -53,14 +53,14 @@ public class CustomerOrderValues {
 
     public static void main(String args[]){
 
-        Scanner sc=new Scanner(System.in);
+        Scanner sca=new Scanner(System.in);
 
-        int n=sc.nextInt();
+        int n=sca.nextInt();
 
         int arr[]=new int[n];
 
         for(int i=0;i<n;i++)
-            arr[i]=sc.nextInt();
+            arr[i]=sca.nextInt();
 
         mergeSort(arr,0,n-1);
 
